43829e6006d2b3640d04e6ce3e28672303f06e7e
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2024-04-02 22:36:58 -0300

    Funções e Eventos
    aguardando ativação da plataforme que disponibiliza a API para poder dar continuidade

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,42 @@
-// 1d7a6557f13578958a732520fa686bc6   chave da api
+//variaveis e seleção de elementos
+const apiKey = "1d7a6557f13578958a732520fa686bc6";
+const apiCountryURL = "https://flagsapi.com//shiny/64.png"
 
-// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={1d7a6557f13578958a732520fa686bc6} teste da API
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+//Funções
+const getWeatherData = async (city) => {
+    toggleLoader();
+  
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+  
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+  
+    toggleLoader();
+  
+    return data;
+
+const showWeartherData = (city) => {
+    cgetWeatherData(city);
+}
+
+//Eventos
+searchBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeartherData(city);
+
+    //console.log("teste")  teste do botão
+});
